97aa214def58f9bd2afeaed6815cb8508d3ae1d1
nat-jpg <98435360+nat-jpg@users.noreply.github.com>
2022-12-11 20:37:29 -0500

    testing
    weather app for Shecodes Plus

---
+++ java.js
@@ -0,0 +1,43 @@
+let now = new Date();
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+
+let date = document.querySelector(".date");
+date.innerHTML = `${day} ${hour}:${minute}`;
+
+function displayWeather(response) {
+  let temperature = document.querySelector("h1");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let location = document.querySelector(".city");
+  location.innerHTML = response.data.name;
+  let condition = document.querySelector("h2");
+  condition.innerHTML = response.data.weather[0].main;
+}
+
+function searchCity(inputCity) {
+  let apiKey = "9171ffc58ca719fcd9afd47b128d0e6f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function city(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector("#city-input").value;
+  searchCity(inputCity);
+}
+
+let inputCity = document.querySelector("#search-city");
+inputCity.addEventListener("submit", city);
+
+  
