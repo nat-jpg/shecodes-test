747858b35e9cfe785a2520d663f42ec5732b6f55
nat-jpg <98435360+nat-jpg@users.noreply.github.com>
2022-12-19 17:28:04 -0500

    Update java.js

--- java.js
+++ java.js
@@ -17,11 +17,11 @@ let date = document.querySelector(".date");
 date.innerHTML = `${day} ${hour}:${minute}`;
 
 function displayWeather(response) {
-  let temperature = document.querySelector("h1");
+  let temperature = document.querySelector(".tempUnit");
   temperature.innerHTML = Math.round(response.data.main.temp);
   let location = document.querySelector(".city");
   location.innerHTML = response.data.name;
-  let condition = document.querySelector("h2");
+  let condition = document.querySelector(".overview");
   condition.innerHTML = response.data.weather[0].main;
 }
 
