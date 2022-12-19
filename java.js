097287453abb8f65166f44956604d869052881a3
nat-jpg <98435360+nat-jpg@users.noreply.github.com>
2022-12-19 18:33:31 -0500

    added  weather icon

--- java.js
+++ java.js
@@ -27,6 +27,8 @@ function displayWeather(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 function searchCity(inputCity) {
