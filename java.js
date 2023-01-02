7cc4eb39706003aa921f378038399cc3922081fd
nat-jpg <98435360+nat-jpg@users.noreply.github.com>
2023-01-02 11:32:25 -0500

    changed api key

--- java.js
+++ java.js
@@ -45,7 +45,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
+  let apiKey = "f5e814a04eddfab1740f07bf0328eee2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
